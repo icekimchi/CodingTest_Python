fb28e55c3875945543245d60456c3df8af67add2
icekimchi <87012362+icekimchi@users.noreply.github.com>
2024-02-02 09:13:53 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 0.33 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.py
+++ 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.py
@@ -11,32 +11,33 @@ def solution(numbers, hand):
         for j, val_j in enumerate(val_i):
             pad[val_j] = (i, j)
 
-    left_pos = (3, 0)
-    right_pos = (3, 2)
+    l_pos = (3, 0)
+    r_pos = (3, 2)
     
     for number in numbers:
         x, y = pad[number]
         if number in left_key:
-            left_pos = x, y
+            l_pos = x, y
             answer += "L"
         elif number in right_key:
-            right_pos = x, y
+            r_pos = x, y
             answer += "R"
         else:
-            right_distance = abs(right_pos[0] - x) + abs(right_pos[1] - y)
-            left_distance = abs(left_pos[0] - x) + abs(left_pos[1] - y)
-            if right_distance<left_distance:
-                right_pos = x, y
+            r_distance = abs(r_pos[0] - x) + abs(r_pos[1] - y)
+            l_distance = abs(l_pos[0] - x) + abs(l_pos[1] - y)
+            
+            if r_distance<l_distance:
+                r_pos = x, y
                 answer += "R"
-            elif right_distance>left_distance:
-                left_pos = x, y
+            elif r_distance>l_distance:
+                l_pos = x, y
                 answer += "L"
             else:
                 if hand=="right":
-                    right_pos = x, y
+                    r_pos = x, y
                     answer += "R"
                 else:
-                    left_pos = x, y
+                    l_pos = x, y
                     answer += "L"
     return answer
 
