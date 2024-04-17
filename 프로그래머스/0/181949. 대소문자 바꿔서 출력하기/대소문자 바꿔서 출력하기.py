f6f8707fa8ce655f78915330fe5084580f2e3440
icekimchi <87012362+icekimchi@users.noreply.github.com>
2024-04-17 23:43:37 +0900

    [level 0] Title: 대소문자 바꿔서 출력하기, Time: 11.37 ms, Memory: 7.33 MB -BaekjoonHub

---
+++ 프로그래머스/0/181949. 대소문자 바꿔서 출력하기/대소문자 바꿔서 출력하기.py
@@ -0,0 +1,11 @@
+str = input()
+ans = ""
+
+for i in range(len(str)):
+    if str[i].isupper():
+        ans += str[i].lower()
+    else:
+        ans += str[i].upper()
+        
+print(ans)
+        
