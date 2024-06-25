17b925caafd7cc1b8076441cff67f7702c54fbd7
icekimchi <87012362+icekimchi@users.noreply.github.com>
2024-06-25 16:42:06 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 0.37 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.py
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.py
@@ -1,27 +1,11 @@
-def del_0(a):
-    cnt = 0
-    new = ""
-    for s in a:
-        if s=="1":
-            new += "1"
-            continue
-        cnt += 1
-    
-    return cnt, new
-
-    
 def solution(s):
-    ans_0 = 0
-    new_s = ""
+    cnt_0 = 0
     ans = 0
     
     while s!="1":
-        tmp_0, new_s = del_0(s)
-        s = bin(len(new_s))[2:]
-        print(s)
-        
-        
-        ans_0 += tmp_0
+        cnt_0 += s.count('0')
+        s = str(len(s) - s.count('0'))
+        s = bin(int(s))[2:]
         ans += 1
     
-    return [ans, ans_0]
+    return [ans, cnt_0]
